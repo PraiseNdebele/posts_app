a5a4135f7526a0cb3a381e0fb4296e75e1d43c06
Praise Ndebele <praise.ndebele@gmail.com>
2015-06-25 18:19:59 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  # Below is your index route, finish filling in the the remaining routes
-  # What method can we use to create all of our RESTful routes at once?
-  get '/posts', to: 'posts#index', as: 'posts'
-
-  # This root route is pointing to the wrong controller, fix it
-  root 'welcome#index'
-
+	resources :posts
+	root 'posts#index'		
 end
